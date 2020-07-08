ed3882aae25cce5527e81829e16463d3a44f433a
Tamas Sztanka-Toth <sztankatt@gmail.com>
2020-07-08 09:59:35 +0200

    removed 10 mil read limit

--- scripts/extract_strand_info.sh
+++ scripts/extract_strand_info.sh
@@ -5,9 +5,9 @@
 # take each read, split the field starting with gf:Z:, and then take whatever is there, and finally count the occurrences. in the end sort by last column
 
 # take only 10 million reads, for faster computation
-MAX_NUM_READS=10000000
+#MAX_NUM_READS=10000000
 
-sambamba view $1 | head -n $MAX_NUM_READS | grep -e 'gs:Z' |\
+sambamba view $1 | grep -e 'gs:Z' |\
     # print the 2nd field (read strand) and the last field (gene strands)
     # extract the strand info from the last field
     awk '{split($NF, gsz, ":")}{print $2" "gsz[3]}' | \
