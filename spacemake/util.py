3aebf97b17d8e0103514277c181874378b329082
marvin-jens <marvin.jens@mdc-berlin.de>
2021-09-30 10:10:45 +0200

    * small refactor to allow (gzipped) FASTQ as well as BAM input into the long-read scripts

--- spacemake/util.py
+++ spacemake/util.py
@@ -49,19 +49,35 @@ def ensure_path(path):
     return path
 
 
-def read_fq(fname):
-    import gzip
+def FASTQ_src(src):
     from more_itertools import grouper
-
-    if fname.endswith(".gz"):
-        src = gzip.open(fname, mode="rt")
-    elif type(fname) is str:
-        src = open(fname)
-    else:
-        src = fname  # assume its a stream or file-like object already
 
     for name, seq, _, qual in grouper(src, 4):
         yield name.rstrip()[1:], seq.rstrip(), qual.rstrip()
+
+
+def BAM_src(src):
+    import pysam
+
+    bam = pysam.AlignmentFile(src, "rb", check_sq=False)
+    for read in bam.fetch(until_eof=True):
+        yield read.query_name, read.query_sequence, read.query_qualities
+
+
+def read_fq(fname):
+    import gzip
+
+    if fname.endswith(".gz"):
+        src = FASTQ_src(gzip.open(fname, mode="rt"))
+    elif fname.endswith(".bam"):
+        src = BAM_src(fname)
+    elif type(fname) is str:
+        src = FASTQ_src(open(fname))
+    else:
+        src = FASTQ_src(fname)  # assume its a stream or file-like object already
+
+    for name, seq, qual in src:
+        yield name, seq, qual
 
 
 def dge_to_sparse(dge_path):
