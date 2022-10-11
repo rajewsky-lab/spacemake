6254443c5f1a46193c34710e1f73120dc13f8507
marvin-jens <marvin.jens@mdc-berlin.de>
2022-10-11 16:48:17 +0200

    * slightly better profiling output

--- prof.py
+++ prof.py
@@ -3,4 +3,4 @@ from pstats import SortKey
 
 p = pstats.Stats("prof_stats")
 # p.strip_dirs().sort_stats(-1).print_stats()
-p.sort_stats(SortKey.TIME).print_stats(10)
+p.strip_dirs().sort_stats(SortKey.TIME).print_stats(100)
