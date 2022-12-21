07e1c1511a5f15c4f37cc75f970a4d90b0beaf27
marvin-jens <marvin.jens@mdc-berlin.de>
2022-12-21 16:03:47 +0100

    * initial round of (memory) optimization. More to come

--- spacemake/bin/complexity.py
+++ spacemake/bin/complexity.py
@@ -9,24 +9,6 @@ import spacemake.util as util
 from spacemake.contrib import __license__, __version__, __author__, __email__
 
 
-def subsample(reads, n):
-    if n == len(reads):
-        sample = reads
-    else:
-        f = float(n) / len(reads)
-        rnd = np.random.random(len(reads))
-        mask = rnd <= f
-        sample = list(reads[mask])
-
-    # print(sample)
-    n_uniq = len(set(sample))
-    return len(sample), n_uniq
-
-
-def get_raw_reads(sample):
-    return int(open(f"counts/{sample}.rawcount.txt").read())
-
-
 def parse_args():
     parser = util.make_minimal_parser(
         prog="complexity.py",
@@ -60,38 +42,55 @@ def parse_args():
     args = parser.parse_args()
     return args
 
+def subsample(reads, n):
+    f = float(n) / len(reads)
+    n_uniq = 0
+    n_reads = 0
+    last = None
+
+    rnd = np.random.random(size=len(reads))
+    mask = rnd <= f
+
+    for r in reads[mask]:
+        n_reads += 1
+        if r != last:
+            n_uniq += 1
+        last = r
+
+    return n_reads, n_uniq
+
 
 def main(args):
     logger = util.setup_logging(args, "spacemake.scripts.complexity")
 
-    reads = defaultdict(list)
-
     logger.info(f"processing '{args.bam}'")
     sam = pysam.AlignmentFile(args.bam, "rb", check_sq=False, threads=2)
-    uniq = set()
+
+    last_qname = None
+    keys = []
     for read in util.timed_loop(sam.fetch(until_eof=True), logger, skim=args.skim):
-        if read.query_name in uniq:
+        if read.query_name == last_qname:
             continue
 
-        uniq.add(read.query_name)
+        last_qname = read.query_name
         cb = read.get_tag("CB")
         umi = read.get_tag("MI")
 
-        key = cb + umi
-        reads["all"].append(key)
+        keys.append(cb + umi)
 
-    logger.info("finished loading all reads")
+    logger.info("finished loading all reads. Sorting")
+    keys = np.array(keys)
+    keys.sort()
+    N_reads = len(keys)
 
-    data = []
-    for tagval, keys in reads.items():
-        keys = np.array(keys, dtype=object)
-        N_reads = len(keys)
+    tagval = "all"
 
-        n_min = 10000
-        if N_reads < 5 * n_min:
-            continue
+    logger.info("sub-sampling and unique sequence counting")
 
+    n_min = 10000
+    if N_reads >= 5 * n_min:
         n_subsamples = np.linspace(n_min, N_reads, 20)
+        data = []
         for n in n_subsamples:
             n, n_UMI = subsample(keys, n)
             logger.debug(
@@ -99,11 +98,11 @@ def main(args):
             )
             data.append((tagval, n, n_UMI, n / n_UMI))
 
-    df = pd.DataFrame(data, columns=["tagval", "n_reads", "n_umis", "pcr_factor"])
-    df["sample"] = args.sample
-    df["bamname"] = os.path.basename(args.bam).replace(".bam", "")
-    df.to_csv(util.ensure_path(args.out_tsv), sep="\t")
-    return df
+        df = pd.DataFrame(data, columns=["tagval", "n_reads", "n_umis", "pcr_factor"])
+        df["sample"] = args.sample
+        df["bamname"] = os.path.basename(args.bam).replace(".bam", "")
+        df.to_csv(util.ensure_path(args.out_tsv), sep="\t")
+        return df
 
 
 if __name__ == "__main__":
