1f9608b54ae50d6fe5f80334b3d261e594236dbe
Daniel León-Perinán <daniel.leonperinan@mdc-berlin.de>
2023-02-10 11:28:54 +0100

    black fmt to n_intersect_sequences

--- spacemake/snakemake/scripts/n_intersect_sequences.py
+++ spacemake/snakemake/scripts/n_intersect_sequences.py
@@ -11,6 +11,7 @@ logger_name = "spacemake.snakemake.scripts.n_intersect_sequences"
 logger = logging.getLogger(logger_name)
 AVAILABLE_CPUS = mp.cpu_count()
 
+
 def setup_parser(parser):
     parser.add_argument(
         "--query",
@@ -23,21 +24,21 @@ def setup_parser(parser):
         "--query-tag",
         type=str,
         help="if input is a bam file, the specified tag will be used as the sequence",
-        default="CB"
+        default="CB",
     )
 
     parser.add_argument(
         "--query-plain-skip",
         type=int,
         help="number of lines to skip, if target input is a plain text file",
-        default=None
+        default=None,
     )
 
     parser.add_argument(
         "--query-plain-column",
         type=int,
         help="column to parse from query, if input is a plain text file",
-        default=None
+        default=None,
     )
 
     parser.add_argument(
@@ -45,7 +46,7 @@ def setup_parser(parser):
         type=str,
         default="\t",
         help="if input is a tabular plain text file, this specifies the character for column separation",
-        choices=['\t', ',', ';'],
+        choices=["\t", ",", ";"],
     )
 
     parser.add_argument(
@@ -68,21 +69,21 @@ def setup_parser(parser):
         "--target-tag",
         type=str,
         help="if input is a bam file, the specified tag will be used as the sequence",
-        default="CB"
+        default="CB",
     )
 
     parser.add_argument(
         "--target-plain-skip",
         type=int,
         help="number of lines to skip, if target input is a plain text file",
-        default=None
+        default=None,
     )
 
     parser.add_argument(
         "--target-plain-column",
         type=int,
         help="column to parse from target, if input is a plain text file",
-        default=None
+        default=None,
     )
 
     parser.add_argument(
@@ -90,7 +91,7 @@ def setup_parser(parser):
         type=str,
         default="\t",
         help="if input is a tabular plain text file, this specifies the character for column separation",
-        choices=['\t', ',', ';'],
+        choices=["\t", ",", ";"],
     )
 
     parser.add_argument(
@@ -103,15 +104,16 @@ def setup_parser(parser):
     parser.add_argument(
         "--n-jobs",
         type=int,
-        help='number of parallel jobs (up to {AVAILABLE_CPUS} in this machine) for database lookup',
-        metavar=f"[0-{AVAILABLE_CPUS-1}]", 
-        choices=range(0,AVAILABLE_CPUS),
+        help="number of parallel jobs (up to {AVAILABLE_CPUS} in this machine) for database lookup",
+        metavar=f"[0-{AVAILABLE_CPUS-1}]",
+        choices=range(0, AVAILABLE_CPUS),
         default=1,
     )
 
     return parser
 
-def BAM_src(src, tag : str = None):
+
+def BAM_src(src, tag: str = None):
     import pysam
 
     bam = pysam.AlignmentFile(src, "rb", check_sq=False)
@@ -121,14 +123,18 @@ def BAM_src(src, tag : str = None):
         else:
             yield None, read.get_tag(tag), None
 
-def plain_src(f : str, skip : int = 0, column : int = 0, separator: str = '\t'):
+
+def plain_src(f: str, skip: int = 0, column: int = 0, separator: str = "\t"):
     reads = pd.read_csv(f, sep=separator, skiprows=skip)
     reads = reads.iloc[:, column]
 
     for read in reads:
         yield None, read, None
 
-def read_with_tag(f: str, tag: str = None, skip: int = None, column : int = 0, separator : str = '\t'):
+
+def read_with_tag(
+    f: str, tag: str = None, skip: int = None, column: int = 0, separator: str = "\t"
+):
     import gzip
 
     if type(f) is not str:
@@ -141,22 +147,32 @@ def read_with_tag(f: str, tag: str = None, skip: int = None, column : int = 0, s
         src = BAM_src(f, tag=tag)
     else:
         src = FASTQ_src(open(f))
-        
+
     for _, seq, _ in src:
         yield seq
+
 
 # TODO: use a partial function to pass args to not use global context
 def find_matches(f):
     global query_seqs, args
-    
+
     start = time.time()
-    target = set(read_with_tag(f, args.target_tag, args.target_plain_skip, args.target_plain_column, args.target_separator))
+    target = set(
+        read_with_tag(
+            f,
+            args.target_tag,
+            args.target_plain_skip,
+            args.target_plain_column,
+            args.target_separator,
+        )
+    )
     n_matches = len(target.intersection(query_seqs))
-    pct_matches_target = n_matches/len(target)
+    pct_matches_target = n_matches / len(target)
     print(f"queried {f} in {(time.time()-start)} s")
 
     return (f, len(target), n_matches, pct_matches_target)
-    
+
+
 @message_aggregation(logger_name)
 def cmdline():
     """cmdline."""
@@ -166,28 +182,41 @@ def cmdline():
 
     parser = argparse.ArgumentParser(
         allow_abbrev=False,
-        description="calculate the number of shared sequences between a query and target(s) file(s)",
+        description="calculate sequence intersection between a query and target(s) file(s)",
     )
     parser = setup_parser(parser)
 
     args = parser.parse_args()
 
     if args.target_id is not None and len(args.target_id) == len(args.target):
-        raise ValueError(f"there is a different number of target_id ({len(args.target_id)}) and target ({len(args.target)})")
+        raise ValueError(
+            f"target_id ({len(args.target_id)}) and target ({len(args.target)}) are different in size"
+        )
 
-    query_seqs = read_with_tag(args.query, args.query_tag, args.query_plain_skip, args.query_plain_column, args.query_separator)
+    query_seqs = read_with_tag(
+        args.query,
+        args.query_tag,
+        args.query_plain_skip,
+        args.query_plain_column,
+        args.query_separator,
+    )
     logger.info(f"read query file from '{args.query}'")
 
     # get unique reads into a global context
     query_seqs = set(query_seqs)
     logger.info(f"hashed unique query reads")
-    logger.info(f"querying against {len(args.target)} targets with {args.n_jobs} parallel jobs")
+    logger.info(
+        f"querying against {len(args.target)} targets with {args.n_jobs} parallel jobs"
+    )
 
     with mp.Pool(args.n_jobs) as pool:
         results = pool.map(find_matches, args.target)
-    
-    df = pd.DataFrame(results, columns=['puck_barcode_file', 'n_barcodes', 'n_matching', 'matching_ratio'])
-    df['puck_barcode_file_id'] = args.target_id
+
+    df = pd.DataFrame(
+        results,
+        columns=["puck_barcode_file", "n_barcodes", "n_matching", "matching_ratio"],
+    )
+    df["puck_barcode_file_id"] = args.target_id
     df.to_csv(args.summary_output, index=False)
 
 
