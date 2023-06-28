325b9ffce2b530436314591e683354a1c79f0e51
marvin-jens <marvin.jens@mdc-berlin.de>
2023-06-28 11:43:25 +0200

    * add the option to call plot_base_freqs with --anchor <regexp/sequence> which would restrict the counting to only reads that have a match and set the 0 coordinate to the 3' end of the match. * also add DataFrame/csv output to keep all data underlying the plots (TODO: refactor completely, such that plot can be generated at a later time)

--- spacemake/bin/plot_base_freqs.py
+++ spacemake/bin/plot_base_freqs.py
@@ -5,6 +5,7 @@ from collections import defaultdict
 import logging
 import matplotlib
 import spacemake.util as util
+import pandas as pd
 
 matplotlib.use("Agg")
 import matplotlib.pyplot as plt
@@ -15,31 +16,43 @@ class NTCounter:
         self.name = name
         self.n = 0
         self.L = 0
+        self.min_x = 0
+        self.max_x = 0
         self.counter = defaultdict(lambda: defaultdict(int))
 
-    def count_seq(self, seq):
+    def count_seq(self, seq, ofs=0):
         self.n += 1
         self.L = max(self.L, len(seq))
+        self.min_x = min(self.min_x, -ofs)
+        self.max_x = max(self.max_x, len(seq) - ofs)
+
         for i, nt in enumerate(seq):
-            self.counter[nt][i] += 1
+            self.counter[nt][i - ofs] += 1
 
     def get_percentages(self, nt):
-        pos = np.arange(self.L)
+        pos = np.arange(self.min_x, self.max_x)
         freq = np.array([self.counter[nt][i] for i in pos])
         if self.n <= 0:
             return np.ones(self.L) * np.nan
         else:
             return 100.0 * freq / float(self.n)
 
-    def plot_percentages(self, ax, legend=True):
+    def get_df(self, bases="ACGTN", **kw):
+        data = dict(kw)
+        data['pos']= np.arange(self.min_x, self.max_x)
+        for nt in bases:
+            data[nt] = self.get_percentages(nt)
+
+        return pd.DataFrame(data).set_index('pos')
+
+    def plot_percentages(self, ax, legend=True, bases = "ACGTN"):
         ax.set_axisbelow(True)
         ax.grid(True, axis="y")
         ax.set_title(f"{self.name}")
 
-        pos = np.arange(self.L)
-        bottom = np.zeros(self.L)
+        pos = np.arange(self.min_x, self.max_x)
+        bottom = np.zeros_like(pos, dtype=float)
 
-        bases = "ACGTN"
         colors = {
             "A": "#f9c734",
             "C": "#F9575A",
@@ -75,6 +88,7 @@ class NTCounter:
         if legend:
             ax.legend(ncol=1, frameon=False, loc="center left", bbox_to_anchor=(1, 0.5))
 
+        ax.set_xlim(self.min_x - 1, self.max_x + 1)
 
 def parse_args():
     parser = util.make_minimal_parser()
@@ -86,7 +100,20 @@ def parse_args():
         help="skim through the file by counting only every n-th read. set to 0 to disable (default=10)",
     )
     parser.add_argument("--out-pdf", default="nt_freq_{sample}.pdf")
+    parser.add_argument("--out-csv", default="nt_freq_{sample}.csv")
+    parser.add_argument("--anchor", default="", help="OPTIONAL: specify an anchoring regexp. Only matching reads will be scanned and the 3' end of the match which will define position 0")
     return parser.parse_args()
+
+def filter_seq(seq, regexp):
+    if not regexp:
+        return 0
+
+    import re
+    M = re.search(regexp, seq)
+    if M:
+        return M.end()
+    else:
+        return False
 
 def main(args):
     sample = os.path.basename(args.fname)
@@ -97,12 +124,18 @@ def main(args):
         counter = NTCounter(sample)
 
         for name, seq, qual in read_fq(args.fname, skim=args.skim):
-            counter.count_seq(seq)
+            x = filter_seq(seq, args.anchor)
+            if x or (not args.anchor):
+                counter.count_seq(seq, ofs=x)
+
+        if args.out_csv:
+            df = counter.get_df(sample=sample, anchor=args.anchor)
+            df.to_csv(args.out_csv, sep='\t')
 
         fig, ax = plt.subplots(figsize=(10, 5))
         counter.plot_percentages(ax)
         fig.tight_layout()
-        fig.savefig(args.pdf.format(sample=sample))
+        fig.savefig(args.out_pdf.format(sample=sample))
 
     elif sample.endswith("bam"):
         logger.debug("reading from BAM")
@@ -119,10 +152,22 @@ def main(args):
             if args.skim and n % args.skim != 0:
                 continue
 
-            R2_counter.count_seq(read.query_sequence)
-            CB_counter.count_seq(read.get_tag("CB"))
-            UMI_counter.count_seq(read.get_tag("MI"))
+            x = filter_seq(read.query_sequence, args.anchor)
+            if x or (not args.anchor):
+                R2_counter.count_seq(read.query_sequence, ofs=x)
+                CB_counter.count_seq(read.get_tag("CB"))
+                UMI_counter.count_seq(read.get_tag("MI"))
 
+        if args.out_csv:
+            df = pd.concat([
+                R2_counter.get_df(name="read2"),
+                CB_counter.get_df(name="BC"),
+                UMI_counter.get_df(name="UMI"),
+            ])
+            df['sample'] = sample
+            df['anchor'] = args.anchor
+            df.to_csv(args.out_csv, sep='\t')
+        
         fig, (ax_r2, ax_cb, ax_umi) = plt.subplots(
             nrows=1,
             ncols=3,
@@ -142,6 +187,8 @@ def main(args):
         logger.info(f"storing barcode & UMI nt-freq profile in '{pdfout}'")
         fig.tight_layout()
         fig.savefig(pdfout)
+    else:
+        raise ValueError(f"unknown file format {args.fname}")
 
 def cmdline():
     args = parse_args()
