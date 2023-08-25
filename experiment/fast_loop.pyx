aabc0982267683aeb7f13e9f792c7c0cd234088f
marvin-jens <marvin.jens@mdc-berlin.de>
2023-08-25 15:45:56 +0200

    * combining FIFOs with fcntl to widen pipe buffers allows to approximate the speed of ... | wc -l. This will be our approach, moving forward!

--- experiment/fast_loop.pyx
+++ experiment/fast_loop.pyx
@@ -5,20 +5,49 @@
 #from types cimport *
 from types import *
 from libc cimport stdlib, stdio
+from typing import TextIO
 
-def distribute(finput, fifos, int rec_size=4, int chunk_size=10000):
+def distribute(str fin_name, list fifo_names, int chunk_size=10000):
     # int fd_input = stdlib.open(finput, mode=stdlib.O_RDONLY)
     # wh
     cdef int i = 0
     cdef int j = 0
-    cdef int n = len(fifos)
+    cdef size_t n_outs = len(fifo_names)
+    cdef size_t n = 0
     cdef str line
+    cdef char* buffer
+    cdef size_t L = 2**20 # 512 kb
+    cdef size_t L_stdin = 2**20 # 1Mb
+    cdef ssize_t n_read
+    buffer = <char*>stdlib.malloc(L)
+    cdef char* fifo_buffers[32]
 
-    for line in finput:
-        j = i % n
-        fifos[j].write(line)
-        # stdio.write(fifos[j].fileno, line)
-        i += 1
+    assert n <= 32
+    cdef stdio.FILE *fifos[32]
 
+    cdef char* stdin_buf = <char*>stdlib.malloc(L_stdin)
+    cdef stdio.FILE *fin = stdio.fopen(fin_name.encode('utf-8'), 'r')
+    stdio.setvbuf(fin, stdin_buf, stdio._IOFBF, L_stdin)
+
+    for i in range(n_outs):
+        fifo_buffers[i] = <char*>stdlib.malloc(L)
+        fifos[i] = stdio.fopen(fifo_names[i].encode('utf-8'), 'w')
+        stdio.setvbuf(fifos[i], fifo_buffers[i], stdio._IOFBF, L)
+
+    while(True):
+        n_read = stdio.getline(&buffer, &L, fin) 
+        if n_read <= 0:
+            break
+
+        j = n // chunk_size
+        stdio.fwrite(buffer, n_read, 1, fifos[j % n_outs])
+        n += 1
+
+    for i in range(n_outs):
+        stdio.fclose(fifos[i])
+        stdlib.free(fifo_buffers[i])
+
+    stdlib.free(buffer)
+    print(n)
 
 
--- experiment/test_pipes.py
+++ experiment/test_pipes.py
@@ -3,11 +3,26 @@ import os
 import multiprocessing as mp
 import pyximport; pyximport.install()
 import fast_loop
+import fcntl
+F_SETPIPE_SZ = 1031  # Linux 2.6.35+
+F_GETPIPE_SZ = 1032  # Linux 2.6.35+
+
+def open_fifo(fifo, mode='rb+'):
+    try:
+        fifo_fd = open(fifo, mode)
+        print("Pipe size            : "+str(fcntl.fcntl(fifo_fd, F_GETPIPE_SZ)))
+        fcntl.fcntl(fifo_fd, F_SETPIPE_SZ, 1000000)
+        print("Pipe (modified) size : "+str(fcntl.fcntl(fifo_fd, F_GETPIPE_SZ)))
+        return fifo_fd
+    except Exception as e:
+        print("Unable to create fifo, error: "+str(e))
+
+fcntl.fcntl(sys.stdin, F_SETPIPE_SZ, 1000000)
 
 def sum_up_from_fifo(fname):
     print(f"fname={fname}")
     s = 0
-    fifo = open(fname, 'rt')
+    fifo = open_fifo(fname, 'rt')
     for line in fifo:
         s += 1
     
@@ -15,33 +30,34 @@ def sum_up_from_fifo(fname):
     print(f"worker {fname}: done")
 
 
-os.mkfifo('fifo1')
-os.mkfifo('fifo2')
+def make_named_pipe(name):
+    if os.path.exists(name):
+        os.remove(name)
+    
+    os.mkfifo(name)
+    
+fnames = ['fifo1', 'fifo2', 'fifo3', 'fifo4']
 
-p1 = mp.Process(target=sum_up_from_fifo, args=('fifo1',))
-p1.start()
+processes = []
+for name in fnames:
+    make_named_pipe(name)
+    p = mp.Process(target=sum_up_from_fifo, args=(name,))
+    p.start()
+    processes.append(p)
 
-p2 = mp.Process(target=sum_up_from_fifo, args=('fifo2',))
-p2.start()
-
-
-fifo1_w = open('fifo1', 'wt')
-fifo2_w = open('fifo2', 'wt')
-
-fifos = [fifo1_w, fifo2_w]
-buffers = [[], []]
-
-fast_loop.distribute(sys.stdin, fifos, rec_size=4, chunk_size=1)
+#make_named_pipe('igzip_input')
+fast_loop.distribute('/dev/stdin', fnames, chunk_size=4)
+# fast_loop.distribute(sys.stdin, ['fifo1', 'fifo2'], rec_size=4, chunk_size=1)
 
 # for i, line in enumerate(sys.stdin):
 #     x = (i // 10000) % 2
 #     fifos[x].write(line)
 
-fifo1_w.close()
-fifo2_w.close()
+#fifo1_w.close()
+#fifo2_w.close()
 
-p1.join()
-p2.join()
+for p in processes:
+    p.join()
+
 print("main: done")
-
 
