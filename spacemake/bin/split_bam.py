ec845e6050ff6bf99e28d3031604b00ac4028c70
marvin-jens <marvin.jens@mdc-berlin.de>
2023-11-27 15:39:02 +0100

    * enhanced split_bam functionality: split by cell-barcode (either prefix-based round robin --mode=cb-split or custom routing --mode=cb-group --cb-demux-table=tsv-file)wq

--- spacemake/bin/split_bam.py
+++ spacemake/bin/split_bam.py
@@ -36,6 +36,24 @@ def sam_distributor(in_pipe, worker_in_pipes, chunk_size=1000):
         el.logger.info(f"finished distributing {n} BAM records.")
 
 
+def sam_CB_distributor(in_pipe, worker_in_pipes, cb_dict={b'A':0, b'C':1, b'G':2, b'T':3, b'N': 0}):
+    with ExceptionLogging("spacemake.parallel.sam_CB_distributor") as el:
+        el.logger.info(
+            f"reading from {in_pipe}, writing to {worker_in_pipes} cb_dict={cb_dict}"
+        )
+
+        sub_size = len(list(cb_dict.keys())[0])
+        n = fast_loop.distribute_by_substr(
+            in_pipe,
+            worker_in_pipes,
+            cb_dict,
+            sub_size,
+            header_detect_func=lambda line: line.startswith("@"),
+            header_broadcast=True,
+        )
+        el.logger.info(f"finished distributing {n} BAM records.")
+
+
 def worker_read_sam_write_bam(in_pipe, out_pipe, mode="hSb", threads=4):
     import os
 
@@ -45,6 +63,53 @@ def worker_read_sam_write_bam(in_pipe, out_pipe, mode="hSb", threads=4):
         os.system(cmd)
 
     el.logger.info(f"finished writing to '{out_pipe}'.")
+
+
+## Helper functions
+def make_cb_prefix_dict(L=4, n=10, include_N=True):
+    lkup = {}
+    for i, prefix in enumerate(util.generate_kmers(L, "ACGTN")):
+        lkup[prefix.encode('ascii')] = i % n
+    
+    return lkup
+
+def load_cb_routing(fname, out_folder, name, ext):
+    import pandas as pd
+    groups_seen = {}
+    n_group = 0
+
+    out_paths = []
+
+    def numeric(key):
+        nonlocal n_group
+        if key in groups_seen:
+            return groups_seen[key]
+        else:
+            groups_seen[key] = n_group
+            n_group += 1
+            out_paths.append(
+                os.path.join(out_folder, f"{name}_{key}{ext}")
+            )
+            return groups_seen[key]
+
+    df = pd.read_csv(fname, sep='\t').set_index('CB').sort_values('group')
+    df['numeric'] = df['group'].apply(numeric)
+    
+    lkup = df['numeric'].to_dict()
+    lkup = dict([(k.encode('ascii'), v) for k, v in lkup.items()])
+    return lkup, out_paths
+
+
+def store_cb_routing(lkup, fname):
+    import pandas as pd
+    # convert bytes to str
+    d = dict([(k.decode('ascii'), v) for k, v in lkup.items()])
+
+    # create two columns
+    df = pd.DataFrame(dict(group=pd.Series(d))).reset_index(names="CB")
+    df.to_csv(fname, sep='\t', index=False)
+
+    return df
 
 
 def parse_args():
@@ -60,6 +125,36 @@ def parse_args():
     parser.add_argument(
         "-n", type=int, default=10, help="number of BAM files to produce (default=10)"
     )
+    parser.add_argument(
+        "--mode", default="rr", choices=['rr', 'cb-split', 'cb-group'], help=(
+            "how would you like your BAM file split up into smaller BAMs?\n\n"
+            "\trr (default)= round robin: cycle between -n <n> output files, record i is assigned to file j = i %% n\n"
+            "\tcb-split = cell-barcode prefix split: read the first --cb-prefix-len <L> characters of the cell barcode "
+            "\tBAM tag 'CB:Z:<cb>' and split the records based on that value among the -n <n> files as evenly as possible."
+            "\tin this mode, the resulting split files are guaranteed to contain non-overlapping sets of barcodes.\n"
+            "\tcb-group = cell-barcode custom routing: read the --cb-demux-list <filename> file to route sets of "
+            "\tpre-defined cell barcodes into specific output files"
+        )
+    )
+    parser.add_argument(
+        "--cb-tag", default='\tCB:Z:', help="how to find the start of the cell barcode BAM tag? default='\tCB:Z:'"
+    )
+    parser.add_argument(
+        "--cb-prefix-len", type=int, default=4,
+    )
+    parser.add_argument(
+        "--cb-demux-table", default="", help=(
+            "provide a tab-separated file with cell-barcode (or CB-prefixes) to out-file association. "
+            "Expects the columns 'CB' and 'group'"
+        )
+    )
+    parser.add_argument(
+        "--cb-demux-out", default="", help=(
+            "store a tab-separated file with cell-barcode (or CB-prefixes) to out-file association. "
+            "only used by --mode cb-split"
+        )
+    )
+
     parser.add_argument(
         "--decompression-threads",
         type=int,
@@ -89,13 +184,28 @@ def split_bam_main():
     base = os.path.dirname(args.bam_in)
     name, ext = os.path.splitext(os.path.basename(args.bam_in))
 
-    out_paths = [
-        os.path.join(args.out_folder, f"{name}_{n:03d}{ext}") for n in range(args.n)
-    ]
 
     # BAM(file) -> [bam_decompressor] -> SAM(FIFO)
     #   -> [sam_distributor] -> n x SAM(FIFO)
     #   -> n x [worker_read_sam_write_bam] -> n x BAM(file)
+
+    out_paths = [
+        os.path.join(args.out_folder, f"{name}_{n:03d}{ext}") for n in range(args.n)
+    ]
+    if args.mode == "rr":
+        distributor = sam_distributor
+        dist_kw = {}
+    elif args.mode == "cb-split":
+        distributor = sam_CB_distributor
+        lkup = make_cb_prefix_dict(args.cb_prefix_len, args.n)
+        dist_kw = {'cb_dict' : lkup}
+        if args.cb_demux_out:
+            store_cb_routing(lkup, args.cb_demux_out)
+
+    elif args.mode == "cb-group":
+        distributor = sam_CB_distributor
+        lkup, out_paths = load_cb_routing(args.cb_demux_table, args.out_folder, name, ext)
+        dist_kw = {'cb_dict': lkup}
 
     with create_named_pipes(pipe_names) as pipe_paths:
         p_decomp = mp.Process(
@@ -104,7 +214,7 @@ def split_bam_main():
             kwargs=dict(threads=args.decompression_threads),
         )
         p_dist = mp.Process(
-            target=sam_distributor, args=(pipe_paths[0], pipe_paths[1:])
+            target=distributor, args=(pipe_paths[0], pipe_paths[1:]), kwargs=dist_kw
         )
         workers = []
         for w_in, w_out in zip(pipe_paths[1:], out_paths):
--- spacemake/cython/fast_loop.pyx
+++ spacemake/cython/fast_loop.pyx
@@ -5,7 +5,7 @@
 #from types cimport *
 from types import *
 from libc cimport stdlib, stdio
-from libc.string cimport memcpy
+from libc.string cimport memcpy, strstr, strcpy
 from typing import TextIO
 
 def distribute(str fin_name, list fifo_names, int chunk_size=10000, size_t in_buf_size=2**20, size_t out_buf_size=2**19, header_detect_func=None, header_fifo=None, header_broadcast=False):
@@ -85,6 +85,107 @@ def distribute(str fin_name, list fifo_names, int chunk_size=10000, size_t in_bu
     stdlib.free(buffer)
 
     return n # number of lines distributed (excluding header)
+
+
+
+def distribute_by_substr(str fin_name, list fifo_names, dict sub_lookup, size_t sub_size, bytes sub_lead=b"\tCB:Z:", size_t in_buf_size=2**20, size_t out_buf_size=2**19, header_detect_func=None, header_fifo=None, header_broadcast=False):
+    cdef size_t i = 0
+    cdef ssize_t j = 0
+    cdef size_t n_outs = len(fifo_names)
+    cdef size_t n = 0
+    cdef str line
+    cdef char* buffer
+    cdef char* sub_lead_c = sub_lead
+    cdef size_t lead_n = len(sub_lead)
+    cdef char* sub_buffer = <char*>stdlib.malloc(sub_size)
+    cdef char* sub_match_c = NULL
+    cdef bytes sub
+    cdef ssize_t n_read = 0
+    # support input distribution to up to 128 fifos in parallel
+    cdef char* fifo_buffers[128]
+    buffer = <char*>stdlib.malloc(in_buf_size)
+
+    assert n <= 128
+    cdef stdio.FILE *fifos[128]
+
+    cdef char* stdin_buf = <char*>stdlib.malloc(in_buf_size)
+    cdef stdio.FILE *fin = stdio.fopen(fin_name.encode('utf-8'), 'r')
+    cdef stdio.FILE *fheader
+    stdio.setvbuf(fin, stdin_buf, stdio._IOFBF, in_buf_size)
+
+    # set up output buffers for the main fifos
+    for i in range(n_outs):
+        fifo_buffers[i] = <char*>stdlib.malloc(out_buf_size)
+        fifos[i] = stdio.fopen(fifo_names[i].encode('utf-8'), 'w')
+        stdio.setvbuf(fifos[i], fifo_buffers[i], stdio._IOFBF, out_buf_size)
+
+    if header_detect_func:
+        # if the input stream contains some special header lines, allow to detect these and write them to a separate header-fifo
+        if header_fifo:
+            fheader = stdio.fopen(header_fifo.encode('utf-8'), 'w')
+
+        while(True):
+            n_read = stdio.getline(&buffer, &in_buf_size, fin) 
+            if n_read <= 0:
+                break
+            
+            if header_detect_func(bytes(buffer).decode("ascii")):
+                if header_fifo:
+                    stdio.fwrite(buffer, n_read, 1, fheader)
+                if header_broadcast:
+                    for i in range(n_outs):
+                        # print(f"writing header line to fifo {i} (n_read={n_read}) line={str(buffer)}")
+                        stdio.fwrite(buffer, n_read, 1, fifos[i])
+            else:
+                # as soon as we see a non-header line, we break and enter the main loop
+                break
+
+        if header_fifo:
+            # close the header fifo
+            stdio.fclose(fheader)
+
+    # main distribute loop
+    while(True):
+        if n_read == 0:
+            # we may have already read a line in the header-detection above!
+            # read a new-line only if n_read has explicitly been zeroed again
+            n_read = stdio.getline(&buffer, &in_buf_size, fin) 
+
+        if n_read <= 0:
+            break
+
+        # extract the \tCB:Z:<cell barcode> value.
+        # for more flexibility, we are going to make '\tCB:Z:' variable, 
+        # and extract sub_n characters downstream of that
+        
+
+        sub_match_c = strstr(buffer, sub_lead_c)
+        if sub_match_c is NULL:
+            raise ValueError(f"can not find {sub_lead} as substring in {str(buffer)}")
+
+        sub = (sub_match_c + lead_n)[:sub_size]
+        # print(f"found sub {sub}")
+
+        j = sub_lookup.get(sub, -1)
+        if j >= 0:
+            stdio.fwrite(buffer, n_read, 1, fifos[j % n_outs])
+
+        n += 1
+        n_read = 0
+
+    # close down all main fifos and free the output buffers
+    for i in range(n_outs):
+        stdio.fclose(fifos[i])
+        stdlib.free(fifo_buffers[i])
+
+    # close the input and free the input buffer, as well as the line buffer
+    stdio.fclose(fin)
+    stdlib.free(stdin_buf)
+    stdlib.free(buffer)
+
+    return n # number of lines distributed (excluding header)
+
+
 
 
 def collect(list fifo_names, str fout_name, int chunk_size=10000, size_t in_buf_size=2**19, size_t out_buf_size=2**20, header_fifo=None):
