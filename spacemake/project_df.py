9fabdb185e0e2156cfb7bac988ba25239bdf3f9c
Daniel León-Perinán <daniel.leonperinan@mdc-berlin.de>
2024-01-22 17:20:21 +0100

    fix species argument for map strategy validation

--- spacemake/project_df.py
+++ spacemake/project_df.py
@@ -1080,9 +1080,14 @@ class ProjectDF:
         # validate and correct map_strategies
         from spacemake.map_strategy import validate_mapstr
         corrected_map_strategies = []
-        # TODO: parse species from the current sample
+
+        if action == "add":
+            _i_species = kwargs["species"]
+        elif action == "update":
+            _i_species = self.df.loc[ix]['species']
+
         corrected_map_strategies.append(
-            validate_mapstr(map_strategy, config=self.config, species=self.df.loc[ix]['species'])
+            validate_mapstr(map_strategy, config=self.config, species=_i_species)
         )
         kwargs['map_strategy'] = corrected_map_strategies
 
