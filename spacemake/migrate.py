71dbe6a2333a0db662ca618a5e4393b9f541daa2
nukappa <nukappa@users.noreply.github.com>
2024-12-19 11:24:50 +0100

    added transfer timestamp

--- spacemake/migrate.py
+++ spacemake/migrate.py
@@ -2,6 +2,7 @@ import os
 import subprocess
 import time
 
+
 def find_bam_files(folder):
     """
     Finds all .bam files in the given folder and checks if any of them is a symlink.
@@ -13,9 +14,33 @@ def find_bam_files(folder):
     
     # Find files and check for symlinks
     bam_files = [f for f in os.listdir(folder) if f.endswith('.bam')]
-    are_symlinks = [os.path.islink(bam_file) for bam_file in bam_files]
+    bam_file_paths = [os.path.join(folder, f) for f in bam_files]
+    are_symlinks = [os.path.islink(bam_file_path) for bam_file_path in bam_file_paths]
 
     return list(zip(bam_files, are_symlinks))
+
+
+def sync_timestamps(original_file, new_file):
+    """
+    Sync the timestamps (access and modification time) of new_file with those of original_file.
+    
+    Args:
+        original_file (str): Path to the file whose timestamps will be copied.
+        new_file (str): Path to the file that will have its timestamps updated.
+    """
+    try:
+        # Get the access time and modification time from original_file
+        source_times = os.stat(original_file)
+
+        # Set the same access and modification time for new_file
+        os.utime(new_file, (source_times.st_atime, source_times.st_mtime))
+
+        print(f"File timestamps of {new_file} set to match {original_file}.")
+    except FileNotFoundError:
+        print(f"Error: One or both of the files '{original_file}' or '{new_file}' do not exist.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
 
 def convert_bam_to_cram(ref_sequence, project_folder, threads=4):
     bam_files = find_bam_files(project_folder)
@@ -23,13 +48,20 @@ def convert_bam_to_cram(ref_sequence, project_folder, threads=4):
     for idx in range(len(bam_files)):
         bam_filename, bam_file_is_symlink = bam_files[idx]
         bam_filename_prefix = bam_filename.rsplit('.', 1)[0]
-        cram_filename = bam_filename_prefix + ".cram"
+        cram_filename = bam_filename_prefix + '.cram'
+
+        if os.path.exists(os.path.join(project_folder, cram_filename)):
+            print('CRAM file', cram_filename, 'already exists. Skipping conversion.')
+            continue
 
         # TODO: change ref sequence for genome, rRNA, phiX, custom?
         
         if bam_file_is_symlink:
-            # TODO: deal with this
-            continue
+            # TODO: fix timestamp for symlink 
+            true_bam_filename = os.readlink(os.path.join(project_folder, bam_filename))
+            true_bam_filename_prefix = true_bam_filename.rsplit('.', 1)[0]
+            os.symlink(true_bam_filename_prefix + '.cram',
+                       os.path.join(project_folder, bam_filename_prefix + '.cram'))
         else:
             print('Converting', bam_filename, 'to', cram_filename, 
             '...', time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
@@ -44,16 +76,15 @@ def convert_bam_to_cram(ref_sequence, project_folder, threads=4):
                 ]
             )
 
-    # TODO: transfer timestamp
+        sync_timestamps(os.path.join(project_folder, bam_filename),
+                        os.path.join(project_folder, cram_filename))
 
-    return
 
 def remove_files():
-    # - BAM files (if CRAMs are present)
+    # - BAM files (only if CRAMs are present)
+    bam_files = find_bam_files(project_folder)
 
     # - unaligned.bam
 
     # remove tiles
-
-    return
 
