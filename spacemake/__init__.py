ef8679ff51ed5d1769546af903e4cd29f2a6f0ca
Marvin Jens <marvin.jens@mdc-berlin.de>
2025-05-27 09:18:49 +0200

    * clean up/add documentation, metadata from .contrib, and check globals() before loading a sub-module.

--- spacemake/__init__.py
+++ spacemake/__init__.py
@@ -1,28 +1,34 @@
-# __version__ = 1.0
-# import matplotlib._path
-# from . import preprocess as pp
-# from . import spatial as sp
-
-# from .smk import Spacemake
 import importlib
+from .contrib import __version__, __author__, __email__, __license__
 
-# implement lazy loading/importing to prevent slow commandline interface
-# the 'mod' returned by import_module() is only really loading the module
-# upon first access to its content. It is also cached through the normal
-# mechanisms of module imports.
+# implement lazy loading/importing to prevent slow commandline interface.
+# we only really load a sub-module upon first access to it.
+# >>> import spacemake # <- fast
+# >>> import spacemake.pl # <- slow (matplotlib pulls in a lot of weight)
+# >>> import spacemake.pl as pl2 # <- fast (cached, returns the same module as above import)
 
-__all__ = ['pl', 'pp', 'sp']
+__all__ = ["pl", "pp", "sp", "__version__", "__author__", "__email__", "__license__"]
 
 _lazy_modules = {
-  'pl' : '.plotting',
-  'pp' : '.preprocess',
-  'sp' : '.spatial',
+    "pl": ".plotting",
+    "pp": ".preprocess",
+    "sp": ".spatial",
 }
 
+
 def __getattr__(name):
-    if name in _lazy_modules:
+    g = globals()
+    if name in g:
+        # we already loaded this before. Don't even go through importlib at all
+        return g[name]
+
+    elif name in _lazy_modules:
+
+        # first access. Need to load
         mod = importlib.import_module(_lazy_modules[name], __name__)
         globals()[name] = mod
         return mod
+
     else:
+        # re-create normal error behavior
         raise AttributeError(f"module {__name__} has no attribute {name}")
