cd3d3a02779d90e17e290b45aa39d295810dcfca
Marvin Jens <marvin.jens@mdc-berlin.de>
2025-06-02 16:06:53 +0200

    * fix splicing of multiple mapped headers with same @PG ID:... records (not the original purpose), e.g. for map_strategy phiX->rRNA

--- spacemake/snakemake/scripts/splice_bam_header.py
+++ spacemake/snakemake/scripts/splice_bam_header.py
@@ -14,6 +14,32 @@ import sys
 import logging
 
 
+def read_header(source):
+    hd = {
+        # "HD": {},
+        "SQ": [],
+        "PG": [],
+        "RG": [],
+    }
+    for line in open(source):
+        if not line.startswith("@"):
+            continue
+
+        parts = line[1:].split("\t")
+        top = parts[0]
+        rec = {}
+        for entry in parts[1:]:
+            key, value = entry.rstrip().split(":", maxsplit=1)
+            rec[key] = value
+
+        if top in ["SQ", "PG", "RG"]:
+            hd[top].append(rec)
+        else:
+            hd[top] = rec
+
+    return hd
+
+
 def print_header(header):
     for k, v in sorted(header.items()):
         if type(v) == dict:
@@ -30,31 +56,72 @@ def print_header(header):
             print(f"@{k}:\t{v}")
 
 
-def unique_IDs(pg_list):
+def sort_PG_list(pg_list):
+    root = None
+    pp = {}
+    for pg in pg_list:
+        if not "PP" in pg:
+            # no Previous Program: we have the first entry
+            root = pg
+        else:
+            pp[pg["PP"]] = pg
+
+    l = [root]
+    pp_id = root["ID"]
+    for i in range(len(pg_list) - 1):
+        pg = pp[pp_id]
+        l.append(pg)
+        pp_id = pg["ID"]
+
+    return l
+
+
+def unique_IDs(pg_list1, pg_list2):
     from collections import defaultdict
 
-    id_counts = defaultdict(int)
+    id_renames = {}
+    ids1 = set([pg["ID"] for pg in pg_list1])
+    for pg in pg_list2:
+        id2 = pg["ID"]
+        id2_ = str(id2)
+        n = 0
+        while id2 in ids1:
+            try:
+                if "." in id2_:
+                    n = int(id2_.split(".")[-1])
+                    id2_ = id2_.rsplit(".", maxsplit=1)[0]
+            except ValueError:
+                pass
 
-    # first, iterate over entire list and count how often each program ID is there.
-    pp_list = [None]
-    if len(pg_list) > 1:
-        pp_list += pg_list[:-1]
+            n += 1
+            id2_ = f"{id2_}.{n}"
 
-    pg_new = []
-    for pg, pp in zip(pg_list, pp_list):
-        name = pg["ID"].split(".")[0]
-        # edit in-place
-        id_counts[name] += 1
-        pg["ID"] = f"{name}.{id_counts[name]}"
-        # id_counts[name] -= 1
+        if id2_ != id2:
+            ids1.add(id2_)
+            id_renames[id2] = id2_
 
-        if pp:
-            pname = pp["ID"].split(".")[0]
-            pg["PP"] = f"{pname}.{id_counts[pname]}"
+    # print("suggest the following renames in pg_list2:")
+    # print(id_renames)
 
-        pg_new.append(pg)
+    return pg_list2
 
-    return pg_new
+
+def merge_PG_lists(orig, other):
+    first = sort_PG_list(orig.copy())
+    last = sort_PG_list(other.copy())
+
+    # print("about to merge PG lists")
+    # for pg in first:
+    #     print("first:", pg)
+
+    # for pg in last:
+    #     print("last:", pg)
+
+    last = unique_IDs(
+        first, last
+    )  # rename re-occuring IDs to make them unique if needed
+    last[0]["PP"] = first[-1]["ID"]  # link the two histories
+    return first + last
 
 
 def merge_headers(orig, other, enforce_RG=True):
@@ -63,8 +130,10 @@ def merge_headers(orig, other, enforce_RG=True):
     # connect the processing chains:
     # most recent program should be on top
     #  Previous Program (PP) of the first new output was the last Program Name (PN) in the original uBAM
-    other["PG"][-1]["PP"] = orig["PG"][0]["ID"]
-    merged["PG"] = unique_IDs(merged["PG"] + other["PG"])
+    # other["PG"][-1]["PP"] = orig["PG"][0]["ID"]
+    merged["PG"] = merge_PG_lists(
+        orig["PG"], other["PG"]
+    )  # unique_IDs(merged["PG"] + other["PG"])
 
     if "SO" in other["HD"]:
         merged["HD"]["SO"] = other["HD"]["SO"]  # keep sort-order
@@ -92,24 +161,35 @@ if __name__ == "__main__":
         help="mapped star/bowtie2 bam input (default=/dev/stdin)",
         default="/dev/stdin",
     )
-    parser.add_argument("--in-ubam", help="unmapped dropseq tagged bam", required=True)
+    parser.add_argument("--in-ubam", help="unmapped dropseq tagged bam")
     parser.add_argument(
         "--out-bam",
         help="fixed output bam (default=/dev/stdout)",
         default="/dev/stdout",
     )
     parser.add_argument("--out-mode", help="mode for output (default=b0)", default="b0")
+    parser.add_argument("--in-header1", help="first header")
+    parser.add_argument("--in-header2", help="second header")
 
     args = parser.parse_args()
 
-    mbam = pysam.AlignmentFile(args.in_bam, "r")
-    ubam = pysam.AlignmentFile(args.in_ubam, "rc", check_sq=False)
+    if args.in_bam and args.in_ubam:
+        mbam = pysam.AlignmentFile(args.in_bam, "r")
+        ubam = pysam.AlignmentFile(args.in_ubam, "rc", check_sq=False)
+        header1 = ubam.header.to_dict()
+        header2 = mbam.header.to_dict()
 
-    mapped_header = mbam.header.to_dict()
-    ubam_header = ubam.header.to_dict()
-    merged_header = merge_headers(ubam_header, mapped_header)
+    if args.in_header1 and args.in_header2:
+        # overwrite headers if so desired
+        header1 = read_header(args.in_header1)
+        header2 = read_header(args.in_header2)
+
+    # print_header(header1)
+    # print_header(header2)
+    merged_header = merge_headers(header1, header2)
+    # print_header(merged_header)
     # print(f"mapped BAM header")
-    # print_header(mapped_header)
+    # print_header(header1)
 
     # print(f"original uBAM header")
     # print_header(ubam_header)
@@ -118,8 +198,9 @@ if __name__ == "__main__":
     # print_header(merged_header)
 
     # copy input to output, just with the new header
-    bam_out = pysam.AlignmentFile(
-        args.out_bam, f"w{args.out_mode}", header=merged_header
-    )
-    for aln in mbam.fetch(until_eof=True):
-        bam_out.write(aln)
+    if args.in_bam:
+        bam_out = pysam.AlignmentFile(
+            args.out_bam, f"w{args.out_mode}", header=merged_header
+        )
+        for aln in mbam.fetch(until_eof=True):
+            bam_out.write(aln)
