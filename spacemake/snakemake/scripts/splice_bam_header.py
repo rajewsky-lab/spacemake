5cf8dc437d3f2cac93fc6c33252728d28766b780
Marvin Jens <marvin.jens@mdc-berlin.de>
2025-06-06 15:50:57 +0200

    * add --repair-header function

--- spacemake/snakemake/scripts/splice_bam_header.py
+++ spacemake/snakemake/scripts/splice_bam_header.py
@@ -1,12 +1,5 @@
 #!/usr/bin/env python3
-__author__ = [
-    "Marvin Jens",
-]
-__license__ = "GPL"
-__email__ = [
-    "marvin.jens@mdc-berlin.de",
-]
-
+from spacemake.contrib import __author__, __email__, __version__
 import pysam
 import argparse
 import os
@@ -15,12 +8,7 @@ import logging
 
 
 def read_header(source):
-    hd = {
-        # "HD": {},
-        "SQ": [],
-        "PG": [],
-        "RG": [],
-    }
+    hd = {}
     for line in open(source):
         if not line.startswith("@"):
             continue
@@ -33,6 +21,8 @@ def read_header(source):
             rec[key] = value
 
         if top in ["SQ", "PG", "RG"]:
+            if not top in hd:
+                hd[top] = []
             hd[top].append(rec)
         else:
             hd[top] = rec
@@ -40,20 +30,113 @@ def read_header(source):
     return hd
 
 
-def print_header(header):
-    for k, v in sorted(header.items()):
+def make_ID_unique(ids1, id2):
+    id2_ = str(id2)
+    n = 0
+    while id2_ in ids1:
+        # print(f"while enter: {id2_}")
+        try:
+            if "." in id2_:
+                n = int(id2_.split(".")[-1])
+                id2_ = id2_.rsplit(".", maxsplit=1)[0]
+        except ValueError:
+            pass
+
+        n += 1
+        id2_ = f"{id2_}.{n}"
+
+    return id2_
+
+
+def repair_PG_lines(pg_list, fix=False):
+    from collections import defaultdict
+
+    issues = []
+
+    no_PP = []
+    w_PP = []
+
+    ids_seen = set()
+    ids_by_cmd = defaultdict(list)
+    rename = {}
+
+    for i, pg in enumerate(pg_list):
+        cmd = pg["ID"].rsplit(".", maxsplit=1)[0]
+        if fix:
+            pg["ID"] = rename.get(pg["ID"], pg["ID"])
+
+        if "PP" in pg:
+            if fix:
+                pg["PP"] = rename.get(pg["PP"], pg["PP"])
+
+            w_PP.append(pg)
+            if pg["ID"] == pg["PP"]:
+                best_guess = ids_by_cmd[cmd][-1]
+                issues.append(
+                    f"entry ID={pg['ID']} references itself: PP={pg['PP']}. Best guess: correct PP={best_guess}"
+                )
+                if fix:
+                    # correct entry is most likely previous run of same tools
+                    pg["PP"] = best_guess
+
+            if pg["PP"] not in ids_seen:
+                issues.append(
+                    f"entry ID={pg['ID']} references PP={pg['PP']}, which has not been defined yet. At a minimum, the ordering is off."
+                )
+        else:
+            no_PP.append(pg)
+            if i > 0:
+                best_guess = pg_list[i - 1]["ID"]
+                issues.append(
+                    f"subsequent PG entry without PP tag ID={pg['ID']}. Have to guess that {no_PP[0]['ID']} is actual beginning. Best guess: correct PP={best_guess}"
+                )
+
+                if fix:
+                    pg["PP"] = best_guess
+                    print(pg["PP"])
+
+        if pg["ID"] in ids_seen:
+            issues.append(f"multiple use of ID {pg['ID']}.")
+            if fix:
+                new_ID = make_ID_unique(ids_seen, pg["ID"])
+                rename[pg["ID"]] = new_ID
+                pg["ID"] = new_ID
+
+        ids_seen.add(pg["ID"])
+        ids_by_cmd[cmd].append(pg["ID"])
+
+    if fix:
+        pg_list = sort_PG_list(pg_list)
+
+    return issues, pg_list
+
+
+def header_to_SAM(header):
+    SAM = []
+    for k, v in header.items():
         if type(v) == dict:
-            vstr = " ".join([f"{x}:{y}" for x, y in sorted(v.items())[::-1]])
-            print(f"@{k}:\t{vstr}")
+            vstr = "\t".join([f"{x}:{y}" for x, y in v.items()])
+            SAM.append(f"@{k}\t{vstr}")
         elif type(v) == list:
             for row in v:
                 if type(row) == dict:
-                    vstr = " ".join([f"{x}:{y}" for x, y in sorted(row.items())[::-1]])
+                    keys = list(row.keys())
+                    if k == "PG":
+                        # override order to ensure CL is always last for max compatibility
+                        keys = [x for x in ["ID", "PN", "VN", "PP", "CL"] if x in row]
+
+                    vstr = "\t".join([f"{x}:{row[x]}" for x in keys])
                 else:
                     vstr = str(row)
-                print(f"@{k}:\t{vstr}")
+                SAM.append(f"@{k}\t{vstr}")
         else:
-            print(f"@{k}:\t{v}")
+            SAM.append(f"@{k}\t{v}")
+
+    return SAM
+
+
+def print_header(header):
+    print("\n".join(header_to_SAM(header)))
 
 
 def sort_PG_list(pg_list):
@@ -142,6 +225,15 @@ def merge_PG_lists(orig, other):
     return first + last
 
 
+def fix_PP(hdr):
+    """
+    Due to a bug in previous versions, we may end up with ID:bowtie2.2 and PP:bowtie2.2
+    in the same PG entry. This breaks samtools view (unless --no-PG is used). So, here we
+    detect this very specific error and fix it.
+    """
+    pass
+
+
 def merge_headers(orig, other, enforce_RG=True):
     merged = dict(orig)
     # start from the original, including VN and RG entries...
@@ -188,8 +280,41 @@ if __name__ == "__main__":
     parser.add_argument("--out-mode", help="mode for output (default=b0)", default="b0")
     parser.add_argument("--in-header1", help="first header")
     parser.add_argument("--in-header2", help="second header")
+    parser.add_argument("--repair-header", default="")
 
     args = parser.parse_args()
+
+    header1 = None
+    header2 = None
+
+    if args.repair_header:
+        bam = pysam.AlignmentFile(args.repair_header, "rc", check_sq=False)
+        hdr = bam.header.to_dict()
+        issues, pg_list = repair_PG_lines(hdr["PG"], fix=True)
+        for iss in issues:
+            print(iss)
+
+        if len(issues) == 0:
+            print("no issues detected.")
+        else:
+            issues_after_fix, pg_list = repair_PG_lines(hdr["PG"], fix=False)
+            assert len(issues_after_fix) == 0
+            print(f"generated fixed header.")
+
+            from tempfile import NamedTemporaryFile
+
+            with NamedTemporaryFile(mode="tw") as tmp_hdr:
+                tmp_hdr = open("bla.sam", "wt")
+                tmp_hdr.write("\n".join(header_to_SAM(hdr)) + "\n")
+                tmp_hdr.flush()
+                tmp_hdr.close()
+                import pysam
+
+                pysam.samtools.reheader("-P", "-i", tmp_hdr.name, args.repair_header)
+                print("replaced original header with fixed header.")
+                # cmd = f"samtools reheader --no-PG -i {tmp_hdr.name} {args.repair_header}"
+                # print(cmd)
+                # print(os.system(cmd))
 
     if args.in_bam and args.in_ubam:
         mbam = pysam.AlignmentFile(args.in_bam, "r")
@@ -204,7 +329,8 @@ if __name__ == "__main__":
 
     # print_header(header1)
     # print_header(header2)
-    merged_header = merge_headers(header1, header2)
+    if header1 and header2:
+        merged_header = merge_headers(header1, header2)
     # print_header(merged_header)
     # print(f"mapped BAM header")
     # print_header(header1)
@@ -216,7 +342,7 @@ if __name__ == "__main__":
     # print_header(merged_header)
 
     # copy input to output, just with the new header
-    if args.in_bam:
+    if args.in_bam and header1 and header2:
         bam_out = pysam.AlignmentFile(
             args.out_bam, f"w{args.out_mode}", header=merged_header
         )
