changed int types to fix potential overflow errors during meshing

spacemake/preprocess/dge.py
@@ -59,7 +59,7 @@ def calculate_shannon_entropy_scompression(adata):
             letter + str(len(list(group)))
             for letter, group in itertools.groupby(barcode)
         )
-        return np.uint8(len(compressed_barcode))
+        return np.uint16(len(compressed_barcode))
 
     bc = adata.obs.index.to_numpy()
     bc_len = len(bc[0])

spacemake/spatial/util.py
@@ -340,7 +340,7 @@ def aggregate_adata_by_indices(
     joined_dict = {i: idx_to_aggregate[x.astype(int)] for i, x in enumerate(ix_array)}
 
     indices_joined_spatial_units = dok_matrix(
-        (len(joined_dict), len(adata.obs_names)), dtype=np.int8
+        (len(joined_dict), len(adata.obs_names)), dtype=np.uint16
     )
 
     for obs_name_aggregate, obs_name_to_aggregate in joined_dict.items():
@@ -366,8 +366,8 @@ def aggregate_adata_by_indices(
 
     aggregated_adata.obs['exact_entropy'] = aggregated_adata.obs['exact_entropy'].astype(np.float16)
     aggregated_adata.obs['theoretical_entropy'] = aggregated_adata.obs['theoretical_entropy'].astype(np.float16)
-    aggregated_adata.obs['exact_compression'] = aggregated_adata.obs['exact_compression'].astype(np.uint8)
-    aggregated_adata.obs['theoretical_compression'] = aggregated_adata.obs['theoretical_compression'].astype(np.uint8)
+    aggregated_adata.obs['exact_compression'] = aggregated_adata.obs['exact_compression'].astype(np.uint16)
+    aggregated_adata.obs['theoretical_compression'] = aggregated_adata.obs['theoretical_compression'].astype(np.uint16)
 
     aggregated_adata.obs['n_counts'] = summarise_adata_obs_column(adata, "n_counts")
     return aggregated_adata